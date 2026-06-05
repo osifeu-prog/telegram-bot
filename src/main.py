d7a3adc680f5f6fbd6902dcb5316818ec6e20413
Osif Kaufman Ungar <   GitHub >
2026-06-05 15:03:35 +0300

    fix railway event loop crash

--- src/main.py
+++ src/main.py
@@ -1,20 +1,23 @@
-import asyncio
+from fastapi import FastAPI
 import uvicorn
+import asyncio
 
-from src.core.bootstrap import bootstrap
 from src.api.app import app
+from src.core.bootstrap import bootstrap
 from src.bot.runner import run_bot
 
 
 def run_api():
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080, log_level="info")
 
 
-async def main():
+async def start_background():
     await bootstrap()
     asyncio.create_task(run_bot())
-    run_api()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    # ? NO asyncio.run
+    loop = asyncio.get_event_loop()
+    loop.create_task(start_background())
+    run_api()
